7082052648df9f029c601f61cf641e1aefc73ced
Dean Johnson <deanjohnson222@gmail.com>
2013-11-13 22:41:03 -0800

    More additions to get rid of errors.

--- proj3/uniqify.c
+++ proj3/uniqify.c
@@ -18,6 +18,7 @@
 #define MAX_LEN 46
 #define BLOCKSIZE 1
 
+char * get_lowest_val_word(FILE *[], int);
 int uniqify(FILE *, int);
 void usage();
 
@@ -63,16 +64,15 @@ int uniqify(FILE * fp, int num_pipes) {
                 printf("Error occured when forking.");
                 break;
             case 0:
+                dup2(fds[i][0], STDIN_FILENO);
                 printf("This is child process %d.\n", i);
                 printf("Pausing process %d.\n", i);
-                pid_t out_fd;
-                //dup2(fds[i][1], out_fd);
-                for (;;_)
-                {
-                    pause();
-                }
-                char * buf;
+
+                kill(getpid(), SIGSTOP);
+
                 printf("Signal recieved for unpausing child process %d.\n", i);
+                execl("/bin/sort", "sort",  (char*) NULL);
+                kill(getpid(), SIGQUIT);
                 // Do stuff like exec'ing.
                 break;
             default:
@@ -85,6 +85,7 @@ int uniqify(FILE * fp, int num_pipes) {
     FILE * open_pipes[num_pipes];
     if (child_pid[0] > 0)
     {
+        sleep(1); // Make sure all processes are spawned
         for(int i=0; i<num_pipes; i++)
         {
             open_pipes[i] = fdopen(fds[i][1], "w");
@@ -101,13 +102,19 @@ int uniqify(FILE * fp, int num_pipes) {
             }
         }
 
+        for(int i=0; i<num_pipes; i++)
+        {
+            if (close(fds[i][0]) == -1)
+            {
+                printf("Error closing pipe %d", i);
+            }
+        }
         // Send out signals to sort pipes
         for(int i=0; i<num_pipes; i++)
         {
             printf("Sending signal for process %d.\n", i);
-            signal(SIGCONT, SIG_IGN);
+            kill(child_pid[i], SIGCONT);
         }
-
 
         int pid;
         // Wait for all processes to finish
@@ -119,9 +126,87 @@ int uniqify(FILE * fp, int num_pipes) {
             }
         }
 
+        for(int i=0; i<num_pipes; i++)
+        {
+            open_pipes[i] = fdopen(fds[i][0] , "r");
+        }
+
+        int word_count = 0;
+        char * prev;
+        char * lowest;
+        while(1)
+        {
+            lowest = get_lowest_val_word(open_pipes, num_pipes);
+            if (strcmp(prev, lowest))
+            {
+                word_count++;
             }
+            else
+            {
+                word_count = 1;
+            }
+
+
+
+            prev = lowest;
+        }
+
+    }
     return 1;
 
+}
+
+char * get_lowest_val_word(FILE* pipes[], int count)
+{
+    char * word;
+    char * word2;
+    fgets(word, MAX_LEN, pipes[0]);
+    if (count > 1)
+    {
+        for (int i=1; i<count; i++)
+        {
+            fgets(word2, MAX_LEN, pipes[i]);
+            int wlen = strlen(word);
+            int wlen2 = strlen(word2);
+
+            int diff = wlen - wlen2;
+            int shorter;
+            if (diff < 0)
+            {
+                shorter = wlen;
+            }
+            else
+            {
+                shorter = wlen2;
+            }
+
+            for (int j=0; j<shorter; j++)
+            {
+                if ((int)word[j] < (int)word2[j])
+                {
+                    break;
+                }
+                else if ((int)word2[j] < (int)word[j])
+                {
+                    word = word2;
+                    break;
+                }
+                else
+                {
+                    if (j == shorter - 1)
+                    {
+                        if (strlen(word) > strlen(word2))
+                        {
+                            word = word2;
+                            break;
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    return word;
 }
 
 void usage() {

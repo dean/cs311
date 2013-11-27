774e5a839747073d77c033f75fc773881f84464f
Dean Johnson <deanjohnson222@gmail.com>
2013-11-27 13:07:24 -0800

    Updated for processes (not working yet)

--- proj4/sevehappy.c
+++ proj4/sevehappy.c
@@ -29,6 +29,7 @@ void * get_happys(void *);
 int get_num_primes(unsigned char *, long);
 int get_next_num(unsigned char *, int, int *, int, unsigned int);
 void get_primes_unthreaded();
+void get_primes_processes(int);
 void get_primes_threaded(int);
 void happy_parallel(unsigned char *, int, unsigned int, int);
 int is_happy(int);
@@ -41,10 +42,10 @@ void usage();
 
 struct bounds {
     unsigned char * bitmap;
-    int base;
-    int * current_nums;
+    int base; // Base number to iterate over
+    int * current_nums; // Numbers other threads are working on.
     int num_threads;
-    int index;
+    int index; // Index for this thread's current_num
     unsigned int lower_bound;
     unsigned int upper_bound;
 };
@@ -65,13 +66,17 @@ int main(int argc, char *argv[])
     }
     int c;
     int num_threads = 0;
-    while ((c = getopt(argc, argv, "ht:")) != -1)
+    while ((c = getopt(argc, argv, "hpt:")) != -1)
     {
         switch(c)
         {
             case 't': // num_threads
                 num_threads = atoi(optarg);
+                get_primes_threaded(num_threads);
                 break;
+            case 'p': // num processes
+                num_threads = atoi(optarg);
+                get_primes_processes(num_threads);
             case 'h': // Provide usage for the command
                 usage();
                 exit(0);
@@ -79,9 +84,22 @@ int main(int argc, char *argv[])
                 break;
         }
     }
-    //get_primes_unthreaded();
-    get_primes_threaded(num_threads);
-    //get_primes_unthreaded();
+    //get_primes_unthreaded(); // Serialized version of the program.
+}
+
+void get_primes_processes(int num_threads) {
+    unsigned char * bitmap = (unsigned char *) malloc((UINT_MAX/BITS_PER_BYTE) + 1);
+    unsigned int upper_bound = UINT_MAX-1;
+
+    printf("Generating Prime List for '%d' threads, up to '%ld'.\n", num_threads, UINT_MAX-1);
+    count_parallel(bitmap, 2, upper_bound, num_threads);
+
+    //printf("Found %d primes", get_num_primes(bitmap, upper_bound));
+
+    //happy_parallel(bitmap, 2, upper_bound, num_threads);
+
+    //printf("Number of happy prime numbers: %d\n", happy_numbers);
+
 }
 
 void get_primes_unthreaded() {
@@ -91,7 +109,6 @@ void get_primes_unthreaded() {
     int num_primes = max;
     for (i=2; i<=sqrt(max); i++) {
         if (marked(bitmap, i) == 0) {
-            printf("Marking Composites for: %d\n", i);
             mark_to_index(bitmap, i, i*2, max);
         }
     }
@@ -122,23 +139,20 @@ void get_primes_unthreaded() {
 
 void get_primes_threaded(int num_threads) {
     unsigned char * bitmap = (unsigned char *) malloc((UINT_MAX/BITS_PER_BYTE) + 1);
-    printf("Addr of bitmap: %p\n", bitmap);
-    printf("Size of bitmap: %ld\n", sizeof(bitmap));
     unsigned int upper_bound = UINT_MAX-1;
 
+    printf("Generating Prime List for '%d' threads, up to '%ld'.\n", num_threads, UINT_MAX-1);
     count_parallel(bitmap, 2, upper_bound, num_threads);
 
-    printf("Done generating prime list...");
     //printf("Found %d primes", get_num_primes(bitmap, upper_bound));
 
-    happy_parallel(bitmap, 2, upper_bound, num_threads);
+    //happy_parallel(bitmap, 2, upper_bound, num_threads);
 
-    /*
-    printf("Number of happy prime numbers: %d\n", happy_numbers);
-    */
-    printf("End.\n");
+    //printf("Number of happy prime numbers: %d\n", happy_numbers);
+
 }
 
+/* Serialized checking of number of primes. */
 int get_num_primes(unsigned char * bitmap, long max) {
     int num_primes = 0;
     for (long i=2; i<max; i++) {
@@ -149,6 +163,7 @@ int get_num_primes(unsigned char * bitmap, long max) {
     return num_primes;
 }
 
+/* Parent function to starting and running all threads for happy number calculations*/
 void happy_parallel(unsigned char * bitmap, int start, unsigned int upper_bound, int num_threads) {
     pthread_t tids[num_threads];
     struct happy_info hi[num_threads];
@@ -170,6 +185,7 @@ void happy_parallel(unsigned char * bitmap, int start, unsigned int upper_bound,
     }
 }
 
+/* Parent spawns off this function for each thread to do happy number calculations */
 void * get_happys(void * ptr) {
     struct happy_info * hi = (struct happy_info *) ptr;
     while (hi->cur_num > 0) {
@@ -184,6 +200,7 @@ void * get_happys(void * ptr) {
     return (void *) NULL;
 }
 
+/* Checks to see if a number is happy */
 int is_happy(int start) {
     int unhappy_numbers[] = {4, 16, 37, 58, 89, 145, 42, 20};
     while (1) {
@@ -200,8 +217,8 @@ int is_happy(int start) {
     return 0;
 }
 
+/* Parent function to starting and running all threads for marking primes*/
 void count_parallel(unsigned char * bitmap, int base, unsigned int upper_bound, int num_threads) {
-    printf("Into count_parallell. b upper: %u %d\n", upper_bound, base);
     pthread_t tids[num_threads];
     struct bounds b[num_threads];
     int * current_nums = (int *) malloc (num_threads * sizeof(int *));
@@ -220,19 +237,26 @@ void count_parallel(unsigned char * bitmap, int base, unsigned int upper_bound, 
         current_num = get_next_num(bitmap, current_num, current_nums, num_threads, UINT_MAX-1);
     }
     for (int i=0; i<num_threads; i++) {
+        //printf("Joining thread '%d'...\n", i);
         pthread_join(tids[i], NULL);
     }
+    return;
 }
 
+/* Gets the next unmarked prime from a bitmap not being used by other threads. */
 int get_next_num(unsigned char * bitmap, int current_num, int * current_nums, int num_threads, unsigned int upper_bound) {
     int max = current_num;
     if (max <= upper_bound) {
         for (int i=max+1; i < upper_bound; i++) {
             if (!marked(bitmap, i)) {
                 int valid_num = 1;
+                // Double checks for repeats.
                 for (int j=0; j < num_threads; j++) {
-                    if ((current_nums[j] >= i) &&
-                            current_nums[j] != 0) {
+                    if (current_nums[j] == 0) {
+                        continue;
+                    }
+                    if (current_nums[j] >= i || current_nums[j] % i == 0 ||
+                            i % current_nums[j] == 0) {
                         valid_num = 0;
                     }
                 }
@@ -242,15 +266,17 @@ int get_next_num(unsigned char * bitmap, int current_num, int * current_nums, in
             }
         }
     }
+    // Threads should exit if there are no more numbers.
     pthread_exit(NULL);
     return -1;
 }
 
+/* Function pass to thread for marking primes. */
 void * mark_between(void * ptr) {
     struct bounds * b = (struct bounds *) ptr;
     while (b->base > 0) {
         b->lower_bound = b->base*2;
-        printf("Marking multiples of index '%d' with thread '%d'!\n", b->base, b->index);
+        //printf("Marking multiples of index '%d' with thread '%d'!\n", b->base, b->index);
         mark_to_index(b->bitmap, b->base, b->lower_bound, b->upper_bound);
         b->base = get_next_num(b->bitmap, b->base, b->current_nums, b->num_threads, (unsigned int) sqrt(UINT_MAX-1));
         b->current_nums[b->index] = b->base;
@@ -259,6 +285,7 @@ void * mark_between(void * ptr) {
     return (void *) NULL;
 }
 
+/* Used in a previous implementation */
 unsigned int adjust_lower_bound(int base, unsigned int lower_bound){
     while(lower_bound % base != 0) {
         lower_bound++;
@@ -266,6 +293,7 @@ unsigned int adjust_lower_bound(int base, unsigned int lower_bound){
     return lower_bound;
 }
 
+/* Gets the sum of squares */
 int sum_square_digits(int num) {
     // Number won't go above 1000
     int sum = 0;
@@ -277,6 +305,7 @@ int sum_square_digits(int num) {
     return sum;
 }
 
+/* Marks primes from start, to end, at interval m on bitmap. */
 void mark_to_index(unsigned char * bitmap, int m, int start, unsigned int end) {
     for (long i = start; i < UINT_MAX-1; i=i+m) {
         if (!marked(bitmap, i)) {
@@ -285,6 +314,7 @@ void mark_to_index(unsigned char * bitmap, int m, int start, unsigned int end) {
     }
 }
 
+/* Checks to see if a bitmap is marked at index ind. */
 bool marked(unsigned char bitmap[], unsigned int ind) {
     unsigned int remainder = ind % BITS_PER_BYTE;
     unsigned int bit_ind = ind / BITS_PER_BYTE;
@@ -293,12 +323,14 @@ bool marked(unsigned char bitmap[], unsigned int ind) {
     return (bitmap[bit_ind] & (1 << remainder)) == pow(2, remainder);
 }
 
+/* Marks a bitmap at index ind. */
 void mark(unsigned char bitmap[], unsigned int ind) {
     unsigned int remainder = ind % BITS_PER_BYTE;
     unsigned int bit_ind = ind / BITS_PER_BYTE;
     bitmap[bit_ind] |= (1 << remainder);
 }
 
+/* Usage message. */
 void usage() {
-    printf("Usage: Don't fuck up.");
+    printf("Usage: \n\t./sevehappy -t <num threads>\n\t./sevehappy -p <num processes>");
 }
